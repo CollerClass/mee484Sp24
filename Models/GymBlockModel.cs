84aac3f7c9f1484753bc2ee11d8dc3f72c24546c
bdcoller <bdcoller@gmail.com>
2024-01-21 00:49:06 -0600

    Initial Commit

---
+++ Models/GymBlockModel.cs
@@ -0,0 +1,24 @@
+//============================================================================
+// GymBlockModel.cs   Geometric model for a generic gymnast. To be used as a
+//                    placeholder until better models are created
+//============================================================================
+using Godot;
+using System;
+
+public partial class GymBlockModel : Node3D
+{
+	
+	
+	//------------------------------------------------------------------------
+	// _Ready: Called once when the node enters the scene tree for the first 
+	//         time.
+	//------------------------------------------------------------------------
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
