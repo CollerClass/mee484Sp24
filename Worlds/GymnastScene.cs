91c1f01e1c226f8791908cd538c77b5c89e06221
bdcoller <bdcoller@gmail.com>
2024-01-21 00:24:08 -0600

    Initial commit.

---
+++ Worlds/GymnastScene.cs
@@ -0,0 +1,20 @@
+//============================================================================
+// GymnastScene.cs   Scene for simulating the spinning gymnast
+//============================================================================
+using Godot;
+using System;
+
+public partial class GymnastScene : Node3D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("MEE 484 - GymnastScene");
+		
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
