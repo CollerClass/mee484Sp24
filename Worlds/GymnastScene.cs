0ebe67f0a1a2eeb37ea24ac2f83398728e3a79b7
bdcoller <bdcoller@gmail.com>
2024-01-21 00:34:07 -0600

    Got some viz

--- Worlds/GymnastScene.cs
+++ Worlds/GymnastScene.cs
@@ -6,14 +6,47 @@ using System;
 
 public partial class GymnastScene : Node3D
 {
-	// Called when the node enters the scene tree for the first time.
+
+
+	CamRig cam;
+	float longitudeDeg;
+	float latitudeDeg;
+	float camDist;
+	float camFOV;
+	Vector3 camTg;       // coords of camera target
+
+	//------------------------------------------------------------------------
+	// _Ready: Called once when the node enters the scene tree for the first 
+	//         time.
+	//------------------------------------------------------------------------
 	public override void _Ready()
 	{
 		GD.Print("MEE 484 - GymnastScene");
+
+		float cgHeight = 1.3f;
+		camTg = new Vector3(0.0f, cgHeight, 0.0f);
+		//model = GetNode<SpinBookModel>("SpinBookModel");
+		//model.SetCGLoc(camTg);
+
+		// Set up the camera rig
+		longitudeDeg = 30.0f;
+		latitudeDeg = 20.0f;
+		camDist = 4.0f;
+		camFOV = 55.0f;  // 40.0f
 		
+		cam = GetNode<CamRig>("CamRig");
+		cam.LongitudeDeg = longitudeDeg;
+		cam.LatitudeDeg = latitudeDeg;
+		cam.Distance = camDist;
+		cam.Target = camTg;
+		//cam.FOVDeg = camFOV;
+
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	//------------------------------------------------------------------------
+	// _Process: Called every frame. 'delta' is the elapsed time since the 
+	//           previous frame.
+	//------------------------------------------------------------------------
 	public override void _Process(double delta)
 	{
 	}
