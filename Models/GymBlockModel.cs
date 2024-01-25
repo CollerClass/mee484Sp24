07a374743354a303b204bcc4e10d1b5340f573d4
bdcoller <bdcoller@gmail.com>
2024-01-24 22:48:29 -0600

    meshes and joints from the pelvis up

--- Models/GymBlockModel.cs
+++ Models/GymBlockModel.cs
@@ -7,7 +7,14 @@ using System;
 
 public partial class GymBlockModel : Node3D
 {
-	
+	Vector3 pelvisAnchor;
+	Vector3 waistRelCoords;
+
+
+	Vector3 lHipRelCoords;
+
+
+	Vector3 pelvisBoxDim;
 	
 	//------------------------------------------------------------------------
 	// _Ready: Called once when the node enters the scene tree for the first 
@@ -15,6 +22,20 @@ public partial class GymBlockModel : Node3D
 	//------------------------------------------------------------------------
 	public override void _Ready()
 	{
+	}
+
+	//------------------------------------------------------------------------
+	// InitBodyCoords: Initialize body coordinates
+	//------------------------------------------------------------------------
+	private void InitBodyCoords()
+	{
+		pelvisAnchor = new Vector3(0.0f, 1.042f, 0.0f);
+		waistRelCoords = new Vector3(0.0f, 0.101f, 0.0f);
+
+
+		lHipRelCoords = new Vector3(0.082f, -0.068f, -0.016f);
+		
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
