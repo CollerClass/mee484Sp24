2c5fff66df2eae7a20066707c794ed6ee9a6e059
bdcoller <bdcoller@gmail.com>
2024-01-31 12:18:12 -0600

    Got camera working

--- Worlds/MannequinScene.cs
+++ Worlds/MannequinScene.cs
@@ -7,12 +7,35 @@ using System;
 public partial class MannequinScene : Node3D
 {
 	
+	CamRig cam;
+	float longitudeDeg;
+	float latitudeDeg;
+	float camDist;
+	float camFOV;
+	Vector3 camTg;       // coords of camera target
+
 	//------------------------------------------------------------------------
 	// _Ready: Called once when the node enters the scene tree for the first 
 	//         time.
 	//------------------------------------------------------------------------
 	public override void _Ready()
 	{
+
+		float cgHeight = 1.3f;
+		camTg = new Vector3(0.0f, cgHeight, 0.0f);
+
+		// Set up the camera rig
+		longitudeDeg = 40.0f;
+		latitudeDeg = 30.0f;
+		camDist = 4.0f;
+		camFOV = 30.0f;  // 40.0f
+		
+		cam = GetNode<CamRig>("CamRig");
+		cam.LongitudeDeg = longitudeDeg;
+		cam.LatitudeDeg = latitudeDeg;
+		cam.Distance = camDist;
+		cam.Target = camTg;
+		cam.FOVDeg = camFOV;
 	}
 
 	//------------------------------------------------------------------------
