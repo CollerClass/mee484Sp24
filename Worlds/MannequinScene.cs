957b01a60f28f266395bf97e2202f7b35d5e8f82
bdcoller <bdcoller@gmail.com>
2024-01-31 01:15:58 -0600

    init commit MannequinScene

---
+++ Worlds/MannequinScene.cs
@@ -0,0 +1,25 @@
+//============================================================================
+// GymnastScene.cs   Scene for simulating the spinning gymnast
+//============================================================================
+using Godot;
+using System;
+
+public partial class MannequinScene : Node3D
+{
+	
+	//------------------------------------------------------------------------
+	// _Ready: Called once when the node enters the scene tree for the first 
+	//         time.
+	//------------------------------------------------------------------------
+	public override void _Ready()
+	{
+	}
+
+	//------------------------------------------------------------------------
+	// _Process: Called every frame. 'delta' is the elapsed time since the 
+	//           previous frame.
+	//------------------------------------------------------------------------
+	public override void _Process(double delta)
+	{
+	}
+}
